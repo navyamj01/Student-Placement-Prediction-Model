fb4146a4d4d40705f8e42c909e137c41f8df63aa
Navyam Jain <navyamjain1503@gmail.com>
2026-06-12 17:31:50 +0530

    cvgh

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import streamlit as st
 import pickle
-from turtle import st
 
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 
-if st.button("Predict"):
+# Get inputs from the user
+CGPA = st.number_input('CGPA', min_value=0.0, max_value=10.0, value=7.0, step=0.01, format="%.2f")
+IQ = st.number_input('IQ', min_value=0, max_value=200, value=100, step=1)
 
-    features = scaler.transform([[cgpa, iq]])
+if st.button("Predict"):
+    features = scaler.transform([[CGPA, IQ]])
     prediction = model.predict(features)
 
     if prediction[0] == 1:
