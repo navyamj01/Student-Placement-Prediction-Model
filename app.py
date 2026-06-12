c9c3714c820a75b44d5c1605bb3b2189a14a8538
Navyam Jain <navyamjain1503@gmail.com>
2026-06-12 17:33:37 +0530

    change

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pickle
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 
+st.title("Student Placement Prediction")
+
 # Get inputs from the user
 CGPA = st.number_input('CGPA', min_value=0.0, max_value=10.0, value=7.0, step=0.01, format="%.2f")
 IQ = st.number_input('IQ', min_value=0, max_value=200, value=100, step=1)
